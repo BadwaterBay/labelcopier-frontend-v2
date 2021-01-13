43a79885ed6cc1aee525e884c827b1fc655ec521
Skyler Dong <dongskyler@gmail.com>
2021-01-12 18:31:27 -0600

    :recycle: Refactor: update default label/milestone values (#8)
    * Every value is a string (partly due to ReactJS)
    * Store original values of existing labels/milestones

--- dummyLabels.js
+++ dummyLabels.js
@@ -3,6 +3,11 @@ const demoLabel0 = {
   name: 'Dummy label 0',
   description: 'This is a dummy label',
   color: '#ffaa00',
+  originalName: 'Dummy label 0',
+  originalDescription: 'This is a dummy label',
+  originalColor: '#ffaa00',
+  action: 'none',
+  validation: 'valid',
 };
 
 const demoLabel1 = {
@@ -10,6 +15,11 @@ const demoLabel1 = {
   name: 'Dummy label 1',
   description: 'This is another dummy label',
   color: '#7722ff',
+  originalName: 'Dummy label 1',
+  originalDescription: 'This is another dummy label',
+  originalColor: '#7722ff',
+  action: 'none',
+  validation: 'valid',
 };
 
 const dummyLabels = [demoLabel0, demoLabel1];
--- dummyMilestones.js
+++ dummyMilestones.js
@@ -1,19 +1,31 @@
 const dummyMilestone0 = {
   id: 'm123456',
-  number: 1,
+  number: '1',
   state: 'open',
   title: 'Dummy milestone 0',
   description: 'This is a dummy milestone',
   dueOn: '2021-07-01',
+  originalState: 'open',
+  originalTitle: 'Dummy milestone 0',
+  originalDescription: 'This is a dummy milestone',
+  originalDueOn: '2021-07-01',
+  action: 'none',
+  validation: 'valid',
 };
 
 const dummyMilestone1 = {
   id: 'm223456',
-  number: 2,
+  number: '2',
   state: 'open',
   title: 'Dummy milestone 1',
   description: 'This is another dummy milestone',
   dueOn: '2021-12-01',
+  originalState: 'open',
+  originalTitle: 'Dummy milestone 1',
+  originalDescription: 'This is another dummy milestone',
+  originalDueOn: '2021-12-01',
+  action: 'none',
+  validation: 'valid',
 };
 
 const dummyMilestones = [dummyMilestone0, dummyMilestone1];
